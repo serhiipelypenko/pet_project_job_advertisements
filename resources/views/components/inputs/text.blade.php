eb052e5bc03f84c643ea649f7d0f4d2245ecb5f7
serhiipelypenko <sergey.pelipenko@triogeeks.com>
2025-02-07 11:51:17 +0200

    Separate inputs in components

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
+@props(['id','name','label'=>null,'type'=>'text','value'=>'', 'placeholder'=>''])
+
+<div class="mb-4">
+    @if($label)
+    <label class="block text-gray-700" for="{{$id}}">{{$label}}</label>
+    @endif
+    <input id="{{$id}}" type="{{$type}}" name="{{$name}}"
+           class="w-full px-4 py-2 border rounded focus:outline-none
+                    @error($name)border-red-500 @enderror"
+           placeholder="{{$placeholder}}" value="{{old($name,$value)}}"/>
+    @error('title')
+    <p class="text-red-500 text-sm mt-1">{{$message}}</p>
+    @enderror
 </div>
