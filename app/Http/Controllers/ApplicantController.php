e04ed8f001376bf982ab9733cb995463f629b083
serhiipelypenko <sergey.pelipenko@triogeeks.com>
2025-02-08 20:16:56 +0200

    Maps And Email

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -6,6 +6,8 @@ use App\Models\Applicant;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Job;
+use App\Mail\JobApplied;
+use Illuminate\Support\Facades\Mail;
 
 class ApplicantController extends Controller
 {
@@ -38,6 +40,9 @@ class ApplicantController extends Controller
            $application->job_id = $job->id;
            $application->user_id = auth()->id();
            $application->save();
+
+           Mail::to($job->user->email)->send(new JobApplied($application, $job));
+
            return redirect()->back()->with('success', 'Your application has been submitted!');
     }
 
